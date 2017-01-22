0b8d56b1f7f5050e4cab15c30ba6776f09269399
coderFK <596448451@qq.com>
2017-01-22 14:05:21 +0800

    jsoup

--- spider/src/com/kai30/AC_QQ.java
+++ spider/src/com/kai30/AC_QQ.java
@@ -1,6 +1,10 @@
 package com.kai30;
 
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.OutputStream;
 
 import org.apache.http.HttpResponse;
 import org.apache.http.ParseException;
@@ -14,37 +18,56 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
-public class Main {
+public class AC_QQ {
 	public static void main(String [] args) throws ClientProtocolException, IOException{
 //		useHttpClient();
 //		useJsoup();
-//		useboth();
-		getTitle();
+		useboth();
+//		getTitle();
 	}
-
+ 
 	private static void getTitle() throws IOException {
 		// TODO Auto-generated method stub
-		Document doc = Jsoup.connect("http://weixin.qq.com").
-				userAgent("Mozilla/5.0 (Windows NT 6.1; rv:22.0) Gecko/20100101 Firefox/22.0").
-				ignoreContentType(true).timeout(30000).get();
+		Document doc = Jsoup.connect("http://ac.qq.com/ComicView/index/id/505432/cid/441").get();
 		Elements ele = doc.select("head title");
 		System.out.println(ele.text());
-		Elements ele_imgUri = doc.getElementsByAttributeValue("rel", "Shortcut Icon");
-		System.out.println(ele_imgUri.attr("href"));
+//		Elements ele_imgUri = doc.getElementsByAttributeValue("rel", "Shortcut Icon");
+//		System.out.println(ele_imgUri.attr("href"));
 		
 	}
 
 	private static void useboth() throws ParseException, IOException {
 		// TODO Auto-generated method stub
 		HttpClient hClient = new DefaultHttpClient();
-		HttpGet hGet = new HttpGet("http://123.206.213.249");
+		HttpGet hGet = new HttpGet("http://ac.qq.com/VIP");
 		HttpResponse response = hClient.execute(hGet);
 		String content = EntityUtils.toString(response.getEntity());
+//		System.out.println(content);
+		
+//		File file = new File("D:\\hy_log.txt");
+//		ByteArrayInputStream bai = new ByteArrayInputStream(content.getBytes());
+//		OutputStream os = new FileOutputStream(file);
+//		int bytesRead = 0;
+//		byte[] buffer = new byte[8192];
+//		while ((bytesRead = bai.read(buffer, 0, 8192)) != -1) {
+//			os.write(buffer, 0, bytesRead);
+//		}
+//		os.close();
+//		bai.close();
+		
 		Document doc = Jsoup.parse(content);
-		Elements ele = doc.getElementsByTag("a");
+		Elements ele = doc.getElementsByClass("in-works-name");
+		
 		for (Element element : ele) {
-			System.out.println(element.attr("href"));
+			System.out.println(element.text());
 		}
+		
+//		Elements ele = doc.select("head script");
+//		System.out.println(ele.text());
+//		Elements ele = doc.getElementsByTag("a");
+//		for (Element element : ele) {
+//			System.out.println(element.attr("href"));
+//		}
 	}
 
 	private static void useJsoup() throws IOException {
